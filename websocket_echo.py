db71b79ef3fc065f61801f7b67216d55b5a07459
42fh <108076931+42fh@users.noreply.github.com>
2024-09-21 21:08:40 +0200

    Added via upload O.O

---
+++ websocket_echo.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# example code form https://websockets.readthedocs.io/en/stable/
+
+import time
+import math
+import asyncio
+from websockets.asyncio.server import serve
+
+print("started ...")
+
+posXY = [10,10]
+lastMsg = time.time()
+
+def foo():
+    t = time.time()
+    numberA = 10 * math.sin(17 * t / 10) + 10.0
+    numberB = 10 * math.cos(4 * t / 10) + 10.0
+    if (time.time() - lastMsg < 3):
+        numberA = posXY[1]
+        numberB = posXY[0]
+    print("numberAB", numberA, numberB)
+    return ("[" + str(numberA) + ", " + str(numberB) + "]")
+
+
+async def echo(websocket):
+    async for message in websocket:
+        if (message != "client"):
+            global posXY
+            global lastMsg
+            posXY = [float(x) for x in message.split(',')]
+            lastMsg = time.time()
+        await websocket.send(foo())
+
+async def main():
+    async with serve(echo, "localhost", 8765):
+        await asyncio.get_running_loop().create_future()  # run forever
+
+asyncio.run(main())
